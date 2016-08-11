0054aeca2f5f802df73474d43770c468e05b3f73
gaganpsinghkrishnais <gaganpsingh@krishnais.com>
2016-08-11 10:24:22 +0530

    Remove extra code in app delegate.

--- iOS_Code/ARA/AUTOAVES_REFERRAL/AppDelegate.m
+++ iOS_Code/ARA/AUTOAVES_REFERRAL/AppDelegate.m
@@ -23,7 +23,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    
     
 //    for(NSString *fontfamilyname in [UIFont familyNames])
 //    {
